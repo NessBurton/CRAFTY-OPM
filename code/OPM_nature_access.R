8bbb6a3272a957bfff0adbdc69e42b0483b821aa
Vanessa Burton <vanessa.burton@ForestResearch.gov.uk>
2020-10-20 14:33:50 +0100

    extract values from nature and access capital rasters and join to hexagonal grid

--- code/OPM_nature_access.R
+++ code/OPM_nature_access.R
@@ -14,18 +14,54 @@ wd <- "~/R/CRAFTY-OPM" # FR
 dirData <- file.path(wd, 'data-raw')
 dirOut <- file.path(wd, 'data-processed')
 
-# GiGL dataset
-GiGL <- st_read(paste0(dirData,"/GiGL_BHP_SHP/GiGL_BHP_region.shp"))
-# clip to AOI
-# rasterise (fine res)
-# sum to gspace polygons
-# extract value per polygon to centroid point
-# normalise values 0-1
+# hex points and grid
+hexPoints <- st_read(paste0(dirOut,"/hexGrids/hexPoints40m.shp"))
+
+# GiGL dataset (clipped to AOI and rasterised at 2m res in Arc)
+bio <- raster(paste0(dirOut,"/rastBHP2m.tif"))
+plot(bio)
 
 # OS open greenspace data
-openGS <- st_read(paste0(dirData,"/OS_open_greenspace_TQ/data/TQ_GreenspaceSite.shp"))
-# join to gspace polygons
-# should be simple new access attribute, 1 if within openGS, 0 if not
+access <- raster(paste0(dirOut,"/rstAccess2m.tif"))
+plot(access)
 
+# extract values 
+# points need to be SpatialPoints
+hexPointsSP <- as_Spatial(hexPoints)
+hexPoints_bio <- extract(bio, hexPointsSP)
+hexPoints_access <- extract(access, hexPointsSP)
+hexPoints_access[which(is.na(hexPoints_access))] <- 0
 
+# combined
+hexP_capitals <- cbind(hexPointsSP,hexPoints_bio, hexPoints_access)
+head(hexP_capitals)
+colnames(hexP_capitals@data)[2] = "nature"
+colnames(hexP_capitals@data)[3] = "access"
+hexP_capitals <- st_as_sf(hexP_capitals)
 
+# join to hexagons
+hexGrid <- st_read(paste0(dirOut,"/hexGrids/hexGrid40m.shp"))
+hexGrid <- as.data.frame(hexGrid)
+hexGrid <- merge(hexGrid, hexP_capitals, by="joinID")
+hexGrid$geometry.y<-NULL # remove poin geometry
+colnames(hexGrid)[2] <- "geometry"
+hexGrid <- st_as_sf(hexGrid)
+
+# check
+ggplot() +
+  geom_sf(hexGrid, mapping = aes(fill = access), col = NA)
+
+# write
+st_write(hexGrid, paste0(dirOut,"/capitals/hexG_bio_access.shp"))
+
+# normalise bio values 0-1
+summary(hexGrid)
+
+data <- hexGrid$nature
+normalised <- (data-min(data))/(max(data)-min(data))
+hist(data)
+hist(normalised)
+
+hexGrid$nature <- normalised
+
+st_write(hexGrid, paste0(dirOut,"/capitals/hexG_bio_access_norm.shp"))
