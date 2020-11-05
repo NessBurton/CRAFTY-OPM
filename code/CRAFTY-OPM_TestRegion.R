9d5d0ad88cf52227caf57c78178099cb1fe0a5f6
Vanessa Burton <vanessa.burton@ForestResearch.gov.uk>
2020-11-05 16:30:06 +0000

    script for producing TestRegion file for CRAFTY

---
+++ code/CRAFTY-OPM_TestRegion.R
@@ -0,0 +1,73 @@
+
+# date: 05/11/2020
+# author: VB
+# description: collate all capital data processed in other scripts and write to a single test region csv for CRAFTY
+
+library(tidyverse)
+library(ggplot2)
+library(sf)
+
+wd <- "C:/Users/vanessa.burton/OneDrive - Forest Research/Documents/R/CRAFTY-OPM"
+
+#####
+# load in capital files
+#####
+
+#opm <- st_read()
+natural <- st_read(paste0(wd,"/data-processed/capitals/hexG_bio_access.shp"))
+social <- st_read(paste0(wd,"/data-processed/capitals/hexG_social.shp"))
+
+#####
+# merge
+#####
+
+social <- social %>% st_drop_geometry()
+TestRegion <- merge(natural,social,by="joinID")
+
+#####
+# extract geometry into x and y
+#####
+
+TestRegion <- TestRegion %>%
+  mutate(X = st_coordinates(st_centroid(.))[,1],
+         Y = st_coordinates(st_centroid(.))[,2]) %>% 
+  st_drop_geometry()
+
+# check plot
+ggplot(TestRegion)+
+  geom_raster(aes(X,Y,fill=nature))
+
+#####
+# edit coordinates so CRAFTY can deal with them (java not a fan of long coordinates)
+#####
+
+# subtract minX from X and minY from Y
+
+xmin <- min(TestRegion$X)
+ymin <- min(TestRegion$Y)
+
+TestRegion$X <- TestRegion$X - xmin
+TestRegion$Y <- TestRegion$Y - ymin
+
+TestRegion$X <- TestRegion$X / 1000
+TestRegion$Y <- round(TestRegion$Y / 1000, digits=2)
+
+ggplot(TestRegion)+
+  geom_tile(aes(X,Y,fill=type))
+
+#####
+# edit order of columns and write to csv
+#####
+
+head(TestRegion)
+head(TestRegion[,c(1,7,8,5,6,2,3)])
+
+# note to self. where 'type' is non-greenspace, mask out/remove from CRAFTY?
+# e.g.
+
+TestRegion <- TestRegion %>%
+  filter(type != "Non.greenspace") 
+
+TestRegion %>%
+  ggplot()+
+  geom_tile(aes(X,Y,fill=type))
