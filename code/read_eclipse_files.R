152e402750fe1fd8269fc97bed92e822203ded80
Vanessa Burton (Sandbox) <vanessa.burton.sb@ForestResearch.gov.uk>
2020-11-11 11:43:17 +0000

    check layout of all files eclipse needs

--- code/read_eclipse_files.R
+++ code/read_eclipse_files.R
@@ -3,5 +3,6 @@ eclipse <- "C:/Users/vanessa.burton.sb/Documents/eclipse-workspace/CRAFTY_OPM/da
 
 TestRegion <- read.csv(paste0(eclipse,"TestRegion.csv"))
 Capitals <- read.csv(paste0(eclipse,"Capitals.csv"))
+Services <- read.csv(paste0(eclipse,"Services.csv"))
 LandUses <- read.csv(paste0(eclipse,"LandUses.csv"))
-AT1 <- read.csv(paste0(eclipse,"AT1.csv"))
+Demand <- read.csv(paste0(eclipse,"Demand1.csv"))
