fc951d9228a885655e626271e25c542dce5c6e16
Vanessa Burton (Sandbox) <vanessa.burton.sb@ForestResearch.gov.uk>
2020-11-11 11:03:54 +0000

    look at eclipse csvs in R

---
+++ code/read_eclipse_files.R
@@ -0,0 +1,7 @@
+
+eclipse <- "C:/Users/vanessa.burton.sb/Documents/eclipse-workspace/CRAFTY_OPM/data/csv/"
+
+TestRegion <- read.csv(paste0(eclipse,"TestRegion.csv"))
+Capitals <- read.csv(paste0(eclipse,"Capitals.csv"))
+LandUses <- read.csv(paste0(eclipse,"LandUses.csv"))
+AT1 <- read.csv(paste0(eclipse,"AT1.csv"))
