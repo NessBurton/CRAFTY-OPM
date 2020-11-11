f4222d62ffc1a78e6d02ca92c019a9b44cd2d879
Vanessa Burton (Sandbox) <vanessa.burton.sb@ForestResearch.gov.uk>
2020-11-11 16:23:10 +0000

    check structure of agent files

--- code/read_eclipse_files.R
+++ code/read_eclipse_files.R
@@ -6,3 +6,5 @@ Capitals <- read.csv(paste0(eclipse,"Capitals.csv"))
 Services <- read.csv(paste0(eclipse,"Services.csv"))
 LandUses <- read.csv(paste0(eclipse,"LandUses.csv"))
 Demand <- read.csv(paste0(eclipse,"Demand1.csv"))
+
+mgmt.high <- read.csv(paste0(eclipse,"mgmt_highInt.csv"))
