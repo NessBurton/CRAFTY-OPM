73e1e41dae164cd2636aac01eef312d6fc6e788b
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2021-02-22 15:06:40 +0000

    old file for creating capital file - ignore

--- code/CRAFTY-OPM_TestRegion.R
+++ code/CRAFTY-OPM_TestRegion.R
@@ -7,42 +7,27 @@ library(tidyverse)
 library(ggplot2)
 library(sf)
 
-#wd <- "C:/Users/vanessa.burton/OneDrive - Forest Research/Documents/R/CRAFTY-OPM"
-wd <- "~/CRAFTY-opm" # sandbox VM
+#dirData <- "C:/Users/vanessa.burton/OneDrive - Forest Research/Documents/R/CRAFTY-OPM"
+dirData <- "~/CRAFTY-opm" # sandbox VM
+scenario <- "de-regulation"
+dirCapitals <- sprintf("~/eclipse-workspace/CRAFTY_RangeshiftR/data_LondonOPM/worlds/LondonBoroughs/%s", scenario)
 
-#####
+
+### baseline -------------------------------------------------------------------
+
 # load in capital files
-#####
+natural <- sf::st_read(paste0(dirData,"/data-processed/capitals/hexG_bio_access.shp"))
+social <- st_read(paste0(dirData,"/data-processed/capitals/hexG_social.shp"))
 
-#opm <- st_read()
-natural <- sf::st_read(paste0(wd,"/data-processed/capitals/hexG_bio_access.shp"))
-social <- st_read(paste0(wd,"/data-processed/capitals/hexG_social.shp"))
-
-#####
 # merge
-#####
-
 social <- social %>% st_drop_geometry()
 TestRegion <- merge(natural,social,by="joinID")
 
-#####
 # add empty OPM presence column for now
-
 TestRegion$OPMpresence <- 0
 
-#####
 # row and column ids for hex
-#####
-
-#install.packages("hexbin")
-#library(hexbin)
-
-
-
-#####
 # extract geometry into x and y
-#####
-
 TestRegion <- TestRegion %>%
   mutate(Long = st_coordinates(st_centroid(.))[,1],
          Lat = st_coordinates(st_centroid(.))[,2]) %>% 
@@ -62,53 +47,24 @@ TRxyz <- TestRegion[,c(11,12,4)]
 rstTestRegion <- rasterFromXYZ(TRxyz)  #Convert first two columns as lon-lat and third as value                
 plot(rstTestRegion)
 
-
 # where 'type' is non-greenspace, mask out/remove from CRAFTY?
-
 TestRegion <- TestRegion %>%
   filter(type != "Non.greenspace") 
-
 TestRegion %>%
   ggplot()+
   geom_tile(aes(Long,Lat,fill=type))
 
 
-#####
 # cell id and lat long
-
 ids <- TestRegion[,c(1,11,12)]
 head(ids)
-write.csv(ids, paste0(wd,"/data-processed/for-CRAFTY/Cell_ID_LatLong.csv"), row.names = F )
-
-#####
-# edit coordinates so CRAFTY can deal with them (java not a fan of long coordinates)
-#####
-
-# subtract minX from X and minY from Y
-
-#xmin <- min(TestRegion$Long)
-#ymin <- min(TestRegion$Lat)
-
-#TestRegion$X <- TestRegion$Long - xmin
-#TestRegion$Y <- TestRegion$Lat - ymin
-
-#TestRegion$X <- TestRegion$X / 1000
-#TestRegion$Y <- round(TestRegion$Y / 1000, digits=2)
-
-#ggplot(TestRegion)+
-  #geom_tile(aes(X,Y,fill=type))
+write.csv(ids, paste0(dirData,"/data-processed/for-CRAFTY/Cell_ID_LatLong.csv"), row.names = F )
 
 
-#####
 # initial agent allocation - no management everywhere
-#####
+TestRegion$Agent <- "no_mgmt"
 
-TestRegion$Agent <- "no_mgmt_NOPM"
-
-#####
 # edit order of columns and write to csv
-#####
-
 head(TestRegion)
 head(TestRegion[,c(1,11,12,13,10,6,8,9,2,3)])
 TestRegion <- TestRegion[,c(1,11,12,13,10,6,8,9,2,3)]
@@ -120,12 +76,12 @@ TestRegion %>%
   ggplot()+
   geom_tile(aes(Long,Lat,fill=riskPerc))
 
-write.csv(TestRegion, paste0(wd,"/data-processed/for-CRAFTY/LondonBoroughs_latLong.csv"), row.names = F)
+write.csv(TestRegion, paste0(dirData,"/data-processed/for-CRAFTY/LondonBoroughs_latLong.csv"), row.names = F)
 
-write.csv(TestRegion, paste0(wd,"/data-processed/for-CRAFTY/TestRegion.csv"), row.names = F)
+write.csv(TestRegion, paste0(dirData,"/data-processed/for-CRAFTY/TestRegion.csv"), row.names = F)
 
 # round coordinates to 1 decimal place for CRAFTY
-TestRegion <- read.csv(paste0(wd,"/data-processed/for-CRAFTY/LondonBoroughs.csv"))
+TestRegion <- read.csv(paste0(dirData,"/data-processed/for-CRAFTY/LondonBoroughs.csv"))
 summary(TestRegion$X)
 TestRegion$X <- round(TestRegion$X, digits=2)
 TestRegion$Y <- round(TestRegion$Y, digits=2)
@@ -135,11 +91,9 @@ summary (TestRegion)
 
 TestRegion <- tibble::rowid_to_column(TestRegion, "id")
 
-write.csv(TestRegion, paste0(wd,"/data-processed/for-CRAFTY/LondonBoroughs.csv"), row.names = F)
+write.csv(TestRegion, paste0(dirData,"/data-processed/for-CRAFTY/LondonBoroughs.csv"), row.names = F)
 
-###
-# 21/01/21
-# create updater file
+### baseline updater file -----------------------------------------------
 
 dirCRAFTYInput <- path.expand(paste0(dirWorking, "/data_LondonOPM/"))
 capitals <- read.csv(paste0(dirCRAFTYInput,"worlds/LondonBoroughs/LondonBoroughs_XY.csv"), header = T, sep = ",")
@@ -168,4 +122,8 @@ for (i in ticks){
 }
 
 
+
+
+
+### de-regulation --------------------------------------------------------------
 
